b8504230688fc70dbfea485d425ec0db5e7d1247
Manikanta AJ <manikantaaj5555@gmail.com>
2025-03-05 12:46:41 +0530

    jenkins file name

--- src/main/java/com/excel/kubernetes/controller/KubernetesController.java
+++ src/main/java/com/excel/kubernetes/controller/KubernetesController.java
@@ -1,5 +1,6 @@
 package com.excel.kubernetes.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class KubernetesController {
 
+    @Value("${spring.fileName}")
+    private String name;
+
     @GetMapping("/message")
     public ResponseEntity<String> getMessage(){
         return ResponseEntity.status(HttpStatus.OK).body("This is Docker Image Page");
+    }
+
+    @GetMapping("/get")
+    public ResponseEntity<String> getAll(){
+        return ResponseEntity.status(HttpStatus.OK).body(name);
     }
 }
